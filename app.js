901f7a6d93e093021886ddd78ac0697282a7f96e
Eugene Gatsak <greenglaz@gmail.com>
2022-07-31 12:18:22 +0400

    fixed ports & config

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = config.get("port") || 5000;
+//const PORT = config.get("port") || 5000;
+
+const PORT = process.env.PORT || 80;
 
 async function start() {
   try {
